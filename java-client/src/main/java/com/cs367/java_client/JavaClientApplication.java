0aa005e860aaac54c2c85b0ec92578afc334c20a
Tonkla <thanapol.tonkla@gmail.com>
2025-05-03 00:40:24 +0700

    Refactor JavaClientApplication to implement HTTP requests for product management

--- java-client/src/main/java/com/cs367/java_client/JavaClientApplication.java
+++ java-client/src/main/java/com/cs367/java_client/JavaClientApplication.java
@@ -1,13 +1,54 @@
 package com.cs367.java_client;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 @SpringBootApplication
 public class JavaClientApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(JavaClientApplication.class, args);
+	
+		public static void main(String[] args) throws Exception {
+			
+			HttpClient client = HttpClient.newHttpClient();
+	
+			
+			HttpRequest request = HttpRequest.newBuilder()
+					.uri(new URI("http://localhost:8081/customer/all-products"))  // เปลี่ยนพอร์ตและ URL ให้ถูกต้อง
+					.build();
+	
+			
+			HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+			System.out.println("Response from customer service: " + response.body());
+	
+			
+			request = HttpRequest.newBuilder()
+					.uri(new URI("http://localhost:8081/customer/reserve-item"))
+					.header("Content-Type", "application/json")
+					.POST(HttpRequest.BodyPublishers.ofString("\"สินค้า A\""))
+					.build();
+	
+			response = client.send(request, HttpResponse.BodyHandlers.ofString());
+			System.out.println("Response from customer service: " + response.body());
+	
+			
+			request = HttpRequest.newBuilder()
+					.uri(new URI("http://localhost:8080/provider/notify-pickup"))
+					.header("Content-Type", "application/json")
+					.POST(HttpRequest.BodyPublishers.ofString("\"สินค้า A\""))
+					.build();
+	
+			response = client.send(request, HttpResponse.BodyHandlers.ofString());
+			System.out.println("Response from provider service: " + response.body());
+	
+			
+			request = HttpRequest.newBuilder()
+					.uri(new URI("http://localhost:8080/provider/reserved-items"))
+					.build();
+	
+			response = client.send(request, HttpResponse.BodyHandlers.ofString());
+			System.out.println("Response from provider service: " + response.body());
+		}
 	}
-
-}
+	
